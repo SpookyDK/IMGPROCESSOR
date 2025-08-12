6465ebcdffd6c61959f0257f5af045736b12792c
SpookyDK <esben9541@gmail.com>
2025-08-12 21:25:13 +0200

    Brightness adjust

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,20 @@ void Rotate_Image_90_Counter(unsigned char* __restrict image, int& width, int& h
         }
     }
 }
+void Adjust_Brightness(unsigned char* __restrict image, int& width, int& height, int& channels, int adjustmeant){
+        int end = width * height * channels;
+        int i = 0;
+        while (i < end){
+            int value0 = static_cast<int>(image[i]) + adjustmeant;
+            int value1 = static_cast<int>(image[i + 1]) + adjustmeant;
+            int value2 = static_cast<int>(image[i + 2]) + adjustmeant;
+            image[i] = static_cast<char>(std::clamp(value0, 0, 255));
+            image[i + 1] = static_cast<char>(std::clamp(value1, 0, 255));
+            image[i + 2] = static_cast<char>(std::clamp(value2, 0, 255));
+            i += channels;
+        }
+        return;
+}
 
 
 
@@ -66,15 +80,22 @@ int main(int argc, char *argv[]) {
     std::cout << "IMG Width:Height:Total(pixels):TotalBytes" << width << ":" << height << ":" << width*height << ":" << width*height*channels*sizeof(data);
     start_time = std::chrono::high_resolution_clock::now();
 
+    start_time = std::chrono::high_resolution_clock::now();
+
+    Adjust_Brightness(data, width, height, channels, 50);
+
+    end_time = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    std::cout << "\nBrightness time = " << duration.count() << " ms\n";
     // QApplication app(argc, argv);
     // QImage qimg(data, width, height, width * 3, QImage::Format_RGB888);
     // QLabel* label = new QLabel;
     // label->setPixmap(QPixmap::fromImage(qimg));
     // label->show();
-
-    end_time = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    std::cout << "\nQT display time = " << duration.count() << " ms\n";
-
+    //
+    // end_time = std::chrono::high_resolution_clock::now();
+    // duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    // std::cout << "\nQT display time = " << duration.count() << " ms\n";
+    //
     // return app.exec();
 }
